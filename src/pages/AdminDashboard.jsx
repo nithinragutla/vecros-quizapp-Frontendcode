892966556f7cecd1476fb1dc5eac26b99257108c
nithin <nithinragutla4@gmail.com>
2025-03-20 02:14:55 +0530

    admin ui

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Adminnavbar from "../components/adminnav";
-import "../styles/AdminDashboard.css"; // Import CSS file
+import "../styles/Admindashboard.css"; 
 
 const AdminDashboard = () => {
     const navigate = useNavigate();
